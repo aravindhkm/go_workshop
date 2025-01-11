cbf03ca3cbe1e7ae87ff9f6f695ea0d5e85c7cfe
aravindhkm <aravindhkathiresan@gmail.com>
2025-01-11 12:47:55 +0530

    Update changes

--- interview/secondLargetNumberOne.go
+++ interview/secondLargetNumberOne.go
@@ -18,7 +18,21 @@ func secondLargetNumberFind(nums []int) int {
 	return secondLargest
 }
 
+func secondApporach(input []int) int{
+	result := make([]int,2)
+
+	for _ , value := range input {
+		if result[0] < value {
+			result[1] =  result[0]
+			result[0] = value
+		} else if result[1] < value {
+			result[1] = value
+		}
+	}
+	return result[1]
+}
+
 func SecondLargetNumberOne() {
 	input := []int{12, 5, 4, 13, 11, 21, 27}
-	fmt.Println("Second Largest Number", secondLargetNumberFind(input))
+	fmt.Println("Second Largest Number", secondApporach(input))
 }
