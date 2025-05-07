114aba6cdbd3a455ab942146700688704934d207
aravindhkm <aravindhkathiresan@gmail.com>
2025-05-07 13:26:12 +0530

    .

--- http_handle/zmain.go
+++ http_handle/zmain.go
@@ -1,6 +1,19 @@
 package httphandle
 
+
+// Store function mappings
+var funcStore = map[string]func(){
+	"1": HttpPostExampleOne,
+	"2": HttpPostExampleTwo,
+}
+
+// Run executes a function by key from the store
 func Run(funcName string) {
-	// HttpPostExampleOne()
-	HttpPostExampleTwo()
+	if fn, exists := funcStore[funcName]; exists {
+		fn()
+		return
+	} else {
+		funcStore["1"]()
+		return
+	}
 }
