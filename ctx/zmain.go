1ff5e424039bc37494481c463b71d1f5d9c6b81d
aravindhkm <aravindhkathiresan@gmail.com>
2025-05-07 13:25:25 +0530

    .

--- ctx/zmain.go
+++ ctx/zmain.go
@@ -1,6 +1,18 @@
 package ctx
 
+// Store function mappings
+var funcStore = map[string]func(){
+	"1": CtxWithValueExampleOne,
+	"2": Diff,
+}
+
+// Run executes a function by key from the store
 func Run(funcName string) {
-	// CtxWithValueExampleOne()
-	Diff()
+	if fn, exists := funcStore[funcName]; exists {
+		fn()
+		return
+	} else {
+		funcStore["1"]()
+		return
+	}
 }
