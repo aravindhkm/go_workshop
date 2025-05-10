e664dda67646a59fffb6f974fa624b52353de58c
aravindhkm <aravindhkathiresan@gmail.com>
2025-05-10 15:35:13 +0530

    .

--- workout/race_condition.go
+++ workout/race_condition.go
@@ -1,29 +1,58 @@
-// package workout
-
-// func RaceCondition() {
-
-// }
-
 package workout
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 var counter int
 
 func RaceCondition() {
-	go increment()
-	go decrement()
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(2)
+	go increment(&wg)
+	go decrement(&wg)
+
+	wg.Wait()
 	fmt.Println("Counter", counter)
 }
 
-func increment() {
+func increment(wg *sync.WaitGroup) {
 	for i := 1; i <= 50; i++ {
 		counter++
 	}
+	wg.Done()
 }
 
-func decrement() {
+func decrement(wg *sync.WaitGroup) {
 	for i := 1; i <= 25; i++ {
 		counter--
 	}
+	wg.Done()
 }
+
+// package workout
+
+// import "fmt"
+
+// var counter int
+
+// func RaceCondition() {
+// 	go increment()
+// 	go decrement()
+// 	fmt.Println("Counter", counter)
+// }
+
+// func increment() {
+// 	for i := 1; i <= 50; i++ {
+// 		counter++
+// 	}
+// }
+
+// func decrement() {
+// 	for i := 1; i <= 25; i++ {
+// 		counter--
+// 	}
+// }
