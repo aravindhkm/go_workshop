67c414b1a4fa4d871d2c41843312ed2a673704a2
aravindhkm <aravindhkathiresan@gmail.com>
2025-05-16 11:42:21 +0530

    .

--- keyword/itr_one.go
+++ keyword/itr_one.go
@@ -1,7 +1,49 @@
 package keyword
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/stretchr/testify/require"
+)
+
+type PaymentMethod interface {
+	Pay(amount int)
+}
+
+func BulkPay(bulkPay []PaymentMethod, payOut []int) {
+	require.Equal(nil, len(bulkPay), len(payOut), "bulkPay and payOut must be of same length")
+
+	for index, value := range payOut {
+		bulkPay[index].Pay(value)
+	}
+}
+
+type CardDetails struct {
+	HolderName string
+	CardNumber int
+	Balance    int
+	Type       string
+}
+
+func (c *CardDetails) Pay(amount int) {
+	c.Balance -= amount
+	fmt.Printf(
+		"User Name: %s, Card Number: %v, Remaining Balance: %d, Card Type: %s\n",
+		c.HolderName, c.CardNumber, c.Balance, c.Type,
+	)
+}
 
 func ItrOne() {
+	creditInfo := &CardDetails{"Aravindh", 14839459384935, 10000, "Credit Card"}
+	debitInfo := &CardDetails{"Aravindh", 14839459384935, 20000, "Debit Card"}
+	gpayInfo := &CardDetails{"Aravindh", 14839459384935, 20000, "G Pay"}
+	paypalInfo := &CardDetails{"Aravindh", 14839459384935, 20000, "PayPal"}
+
+	paymentMethods := []PaymentMethod{creditInfo, debitInfo, gpayInfo, paypalInfo}
+	payments := []int{10, 20, 30, 40}
+
+	BulkPay(paymentMethods, payments)
+
+
 	fmt.Println("Interface")
 }
