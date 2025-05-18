7c78db4dda4e5384ebc39e778fdae03d49516f3a
aravindhkm <aravindhkathiresan@gmail.com>
2025-05-18 18:32:56 +0530

    .

--- goroutine_work/goroutine_one.go
+++ goroutine_work/goroutine_one.go
@@ -5,15 +5,21 @@ import (
 	"net/http"
 )
 
-func MakeCall(urls []string) {
-	for _, endpoint := range urls {
-		res, err := http.Get(endpoint)
-		if err != nil {
-			fmt.Printf("Error fetching URL %s: %v\n", endpoint, err)
-		}
+type Result struct {
+	endpoint   string
+	statusCode int
+}
 
-		fmt.Println(endpoint, res.StatusCode)
+func MakeCall(url string, ch chan Result) {
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error fetching URL %s: %v\n", url, err)
 	}
+
+	// fmt.Println(url, res.StatusCode)
+	tmpRes := Result{url, res.StatusCode}
+	ch <- tmpRes
+
 }
 func GoRoutineOne() {
 	urls := []string{
@@ -22,5 +28,19 @@ func GoRoutineOne() {
 		"https://httpbin.org/get",
 	}
 
-	MakeCall(urls)
+	ch := make(chan Result)
+
+	for _, endpoint := range urls {
+		go MakeCall(endpoint, ch)
+	}
+
+	// channel receiver is a blocking area
+
+	fmt.Println("end result 1", <-ch)
+
+
+	// fmt.Println("end result 2", <-ch)
+	// fmt.Println("end result 3", <-ch)
+	// fmt.Println("end result 3", <-ch)
+
 }
