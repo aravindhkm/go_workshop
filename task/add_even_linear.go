96d74804da600aa8a81c53b95d743df38d1e74f4
aravindhkm <aravindhkathiresan@gmail.com>
2025-05-19 11:08:16 +0530

    .

--- task/add_even_linear.go
+++ task/add_even_linear.go
@@ -7,19 +7,23 @@ import (
 
 func printOdd(oddCh, evenCh chan bool, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for i := 1; i <= 9; i += 2 {
-		fmt.Println("Odd:", i)
-		<-oddCh        		// block odd until receive
-		evenCh <- true 		// release the even block
+	for i := 1; i <= 10; i++ {
+		if i%2 != 0 {
+			fmt.Println(i)
+			<-oddCh        // block odd until receive
+			evenCh <- true // release the even block
+		}
 	}
 }
 
 func printEven(oddCh, evenCh chan bool, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for i := 2; i <= 10; i += 2 {
-		oddCh <- true 			// release the odd block
-		fmt.Println("Even:", i)
-		<-evenCh 				// block even until receive
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			oddCh <- true // release the odd block
+			fmt.Println(i)
+			<-evenCh // block even until receive
+		}
 	}
 }
 
@@ -33,3 +37,34 @@ func AddEvenLiner() {
 
 	wg.Wait()
 }
+
+// working program
+
+// func printOdd(oddCh, evenCh chan bool, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	for i := 1; i <= 9; i += 2 {
+// 		fmt.Println(i)
+// 		<-oddCh        		// block odd until receive
+// 		evenCh <- true 		// release the even block
+// 	}
+// }
+
+// func printEven(oddCh, evenCh chan bool, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	for i := 2; i <= 10; i += 2 {
+// 		oddCh <- true 			// release the odd block
+// 		fmt.Println(i)
+// 		<-evenCh 				// block even until receive
+// 	}
+// }
+
+// func AddEvenLiner() {
+// 	oddCh, evenCh := make(chan bool), make(chan bool)
+// 	var wg sync.WaitGroup
+// 	wg.Add(2)
+
+// 	go printOdd(oddCh, evenCh, &wg)
+// 	go printEven(oddCh, evenCh, &wg)
+
+// 	wg.Wait()
+// }
