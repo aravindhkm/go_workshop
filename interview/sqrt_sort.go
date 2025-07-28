c7272d9e741a19b0e70f266edc129c0799971782
aravindhkm <aravindhkathiresan@gmail.com>
2025-07-28 16:03:31 +0530

    .

---
+++ interview/sqrt_sort.go
@@ -0,0 +1,10 @@
+package interview
+
+func SqrtSort() {
+
+}
+
+// Given an integer array nums sorted in non-decreasing order,
+// return an array of the squares of each number sorted in non-decreasing order.
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
